53e3a9ec096078ea6a5094f6f9e6e692239c792f
feint123 <1084645640@qq.com>
2017-06-18 00:29:11 +0800

    change

---
+++ src/main/java/com/feint/utilities/FileUtils.java
@@ -0,0 +1,27 @@
+package com.feint.utilities;
+
+import java.net.URL;
+import java.net.URLConnection;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class FileUtils{
+    public static boolean download(URL url,String filePath)throws Exception{
+        InputStream input=url.openConnection().getInputStream();
+
+        File file=new File(filePath);
+        FileOutputStream fileOut=new FileOutputStream(file);
+
+        int len=0;
+        byte[]  bs=new byte[1024];
+
+        while((len=input.read(bs))!=-1){
+            fileOut.write(bs);
+            fileOut.flush();
+        }
+
+        input.close();
+        fileOut.close();
+    }
+}
