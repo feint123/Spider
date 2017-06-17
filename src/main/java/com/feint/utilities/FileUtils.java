4d4713aa7d48bcf7f1ac4b0c19641c2d1f86d143
feint123 <13479399352zyf@gmail.com>
2017-06-18 00:32:34 +0800

    change

--- src/main/java/com/feint/utilities/FileUtils.java
+++ src/main/java/com/feint/utilities/FileUtils.java
@@ -1,5 +1,6 @@
 package com.feint.utilities;
 
+import java.io.File;
 import java.net.URL;
 import java.net.URLConnection;
 import java.io.FileOutputStream;
@@ -7,7 +8,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class FileUtils{
-    public static boolean download(URL url,String filePath)throws Exception{
+    public static void download(URL url,String filePath)throws Exception{
         InputStream input=url.openConnection().getInputStream();
 
         File file=new File(filePath);
